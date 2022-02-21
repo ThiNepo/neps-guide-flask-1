459d4df9f272cb90c47ccc11d07064638a38b6f8
Thiago Nepomuceno <thi.nepo@gmail.com>
2022-02-21 10:53:28 -0300

    feat: chend to getcwd to work on exe

--- API_course/config.py
+++ API_course/config.py
@@ -1,6 +1,7 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.getcwd()
 
 
 class Config:
@@ -12,7 +13,7 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"].replace(
     #     "postgres", "postgresql"
     # )
-    
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "t5rbggFvME54t3D@")
